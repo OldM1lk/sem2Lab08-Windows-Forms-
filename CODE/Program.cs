e8be3a72b5521f42859370a58601d4aaed772103
OldM1lk <144899273+OldM1lk@users.noreply.github.com>
2024-04-17 13:31:23 +0700

    Add code and exe files

---
+++ CODE/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+    interface IView
+    {
+        string FirstDirectory();
+        string SecondDirectory();
+
+        void Synchronize(List<string> messages);
+
+        event EventHandler<EventArgs> SynchronizeDirectories;
+    }
+
+    class Model
+    {
+        public List<string> SynchronizingDirectories(string firstDirectory,  string secondDirectory)
+        {
+            DirectoryInfo sourceDirectory = new DirectoryInfo(firstDirectory);
+            DirectoryInfo destinationDirectory = new DirectoryInfo(secondDirectory);
+
+            List<string> synchronizationResult;
+
+            synchronizationResult = InternalDirectorySynchronization(sourceDirectory, destinationDirectory);
+
+            return synchronizationResult;
+        }
+
+        private List<string> InternalDirectorySynchronization(DirectoryInfo firstDirectory, DirectoryInfo secondDirectory)
+        {
+            List<string> resultOfInternalDirectorySynchronization = new List<string>();
+
+            foreach (FileInfo file in firstDirectory.GetFiles())
+            {
+                FileInfo fileInFirstDirectory = new FileInfo(Path.Combine(secondDirectory.FullName, file.Name));
+
+                if (!fileInFirstDirectory.Exists || fileInFirstDirectory.LastWriteTime < file.LastWriteTime)
+                {
+                    File.Copy(file.FullName, fileInFirstDirectory.FullName, true);
+                    resultOfInternalDirectorySynchronization.Add($"Файл {file.Name} изменен");
+                }
+            }
+
+            foreach (FileInfo file in secondDirectory.GetFiles())
+            {
+                FileInfo fileInSecondDirectory = new FileInfo(Path.Combine(firstDirectory.FullName, file.Name));
+
+                if (!fileInSecondDirectory.Exists)
+                {
+                    file.Delete();
+                    resultOfInternalDirectorySynchronization.Add($"Файл {file.Name} удален");
+                }
+            }
+            return resultOfInternalDirectorySynchronization;
+        }
+    }
+
+    class Presenter
+    {
+        private IView view;
+        private Model model;
+        public Presenter(IView newView)
+        {
+            view = newView;
+            model = new Model();
+
+            view.SynchronizeDirectories += new EventHandler<EventArgs>(Synchronize);
+        }
+
+        private void Synchronize(object sender, EventArgs newEvent)
+        {
+            List<string> resultOfSynchronization = model.SynchronizingDirectories(view.FirstDirectory(), view.SecondDirectory());
+            view.Synchronize(resultOfSynchronization);
+        }
+    }
+
+    class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new View());
+        }
+    }
+}
---
+++ CODE/View.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+    public partial class View : Form, IView
+    {
+        public View()
+        {
+            InitializeComponent();
+            Presenter presenter = new Presenter(this);
+        }
+
+        string IView.FirstDirectory() { return firstDirectoryTextBox.Text; }
+        string IView.SecondDirectory() { return secondDirectoryTextBox.Text; }
+        
+        void IView.Synchronize(List<string> messages)
+        {
+            Results.Items.Clear();
+            List<string> outputList = messages;
+
+            foreach (string output in outputList)
+            {
+                Results.Items.Add(output);
+            }
+        }
+
+        public event EventHandler<EventArgs> SynchronizeDirectories;
+
+        private void SynchronizeButton_Click(object sender, EventArgs newEvent)
+        {
+            SynchronizeDirectories(sender, newEvent);
+        }
+    }
+}
